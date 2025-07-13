222fa4ad22572a9331711efb3220acd7ea4077d4
mmmaghool <mostafa.mono.end2023@gmail.com>
2025-07-13 03:52:43 -0800

    3 commit

--- config/settings.py
+++ config/settings.py
@@ -69,10 +69,8 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 # context_processor
                 'post.context_processors.recent_posts'
+                
             ],
-        },
-        "django": {
-            "settingsFile": "Hoomas/settings.py"
         },
     },
 ]
@@ -126,6 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
